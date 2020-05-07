1ae03c5395bb7aa94f51d4b8f853f0881a21cc49
ahavamorse <ahavamorse@icloud.com>
2020-05-07 12:01:56 -0700

    Finished with signUp and signIn user and API flows. User can now create a new account and sign in with their credentials.

--- AnimalSpotter/Controllers/APIController.swift
+++ AnimalSpotter/Controllers/APIController.swift
@@ -21,6 +21,7 @@ final class APIController {
     }
     
     private let baseURL = URL(string: "https://lambdaanimalspotter.vapor.cloud/api")!
+    var bearer: Bearer?
     
     private lazy var signUpURL = baseURL.appendingPathComponent("/users/signup")
     private lazy var signInURL = baseURL.appendingPathComponent("/users/login")
@@ -47,11 +48,77 @@ final class APIController {
         
         // set up data task and handle response
         URLSession.shared.dataTask(with: request) { (_, response, error) in
-            // handle response
+            // handle errors (like no internet connectivity, or anything that generates an Error object)
+            if let error = error {
+                completion(error)
+                return
+            }
+            
+            // handle client errors and server errors that generate non 200 status codes
+            if let response = response as? HTTPURLResponse,
+            response.statusCode != 200 {
+                completion(NSError(domain: "", code: response.statusCode, userInfo: nil))
+                return
+            }
+            
+            // if we get this far, the response contained no errors, so sign up was successful
+            completion(nil)
         }.resume()
     }
     
     // create function for sign in
+    func signIn(with user: User, completion: @escaping (Error?) -> ()) {
+        // create a URLRequest from endpoint specific URL
+        var request = URLRequest(url: signInURL)
+        
+        // modify the request for POST, add proper headers
+        request.httpMethod = HTTPMethod.post.rawValue
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        
+        // encode the user model to JSON, attach as request body
+        let jsonEncoder = JSONEncoder()
+        do {
+            let jsonData = try jsonEncoder.encode(user)
+            request.httpBody = jsonData
+        } catch {
+            NSLog("Error encoding user object: \(error)")
+            completion(error)
+            return
+        }
+        
+        // set up data task and handle response
+        URLSession.shared.dataTask(with: request) { (data, response, error) in
+            // handle errors (like no internet connectivity, or anything that generates an Error object)
+            if let error = error {
+                completion(error)
+                return
+            }
+            
+            // handle client errors and server errors that generate non 200 status codes
+            if let response = response as? HTTPURLResponse,
+            response.statusCode != 200 {
+                completion(NSError(domain: "", code: response.statusCode, userInfo: nil))
+                return
+            }
+            
+            guard let data = data else {
+                completion(NSError())
+                return
+            }
+            
+            let decoder = JSONDecoder()
+            do {
+                self.bearer = try decoder.decode(Bearer.self, from: data)
+            } catch {
+                NSLog("Error decoding bearer object: \(error)")
+                completion(error)
+                return
+            }
+            
+            // if we get this far, the response contained no errors, so log in was successful
+            completion(nil)
+        }.resume()
+    }
     
     // create function for fetching all animal names
     
