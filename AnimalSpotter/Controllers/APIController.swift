00c4ddd178dd1890cc95921cf64c8baf3d5e55b3
ahavamorse <ahavamorse@icloud.com>
2020-05-07 10:26:37 -0700

    Set up signUp endpoint method and http request

--- AnimalSpotter/Controllers/APIController.swift
+++ AnimalSpotter/Controllers/APIController.swift
@@ -10,21 +10,46 @@ import Foundation
 import UIKit
 
 final class APIController {
-
-enum HTTPMethod: String {
-    case get = "GET"
-    case post = "POST"
-}
-
-enum NetworkError: Error {
-    case noData
-}
-
-private let baseURL = URL(string: "https://lambdaanimalspotter.vapor.cloud/api")!
-private lazy var signUpURL = baseURL.appendingPathComponent("/users/signup")
-private lazy var signInURL = baseURL.appendingPathComponent("/users/login")
+    
+    enum HTTPMethod: String {
+        case get = "GET"
+        case post = "POST"
+    }
+    
+    enum NetworkError: Error {
+        case noData
+    }
+    
+    private let baseURL = URL(string: "https://lambdaanimalspotter.vapor.cloud/api")!
+    
+    private lazy var signUpURL = baseURL.appendingPathComponent("/users/signup")
+    private lazy var signInURL = baseURL.appendingPathComponent("/users/login")
     
     // create function for sign up
+    func signUp(with user: User, completion: @escaping (Error?) -> ()) {
+        // create a URLRequest from endpoint specific URL
+        var request = URLRequest(url: signUpURL)
+        
+        // modify the request for POST, add proper headers
+        request.httpMethod = HTTPMethod.post.rawValue
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        
+        // encode the user model to JSON, attach as request body
+        let jsonEncoder = JSONEncoder()
+        do {
+            let jsonData = try jsonEncoder.encode(user)
+            request.httpBody = jsonData
+        } catch {
+            NSLog("Error encoding user object: \(error)")
+            completion(error)
+            return
+        }
+        
+        // set up data task and handle response
+        URLSession.shared.dataTask(with: request) { (_, response, error) in
+            // handle response
+        }.resume()
+    }
     
     // create function for sign in
     
